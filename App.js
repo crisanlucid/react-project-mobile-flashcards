17058fcc2d0dc544267b914e6ca6005885cc774b
crisanlucid <crisan.lucid@yahoo.com>
2020-04-26 16:24:13 +0200

    feat: Show data in App Component

--- App.js
+++ App.js
@@ -1,12 +1,27 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { getDecks } from './utils/api';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-    </View>
-  );
+export default class App extends Component {
+  state = {
+    data: '',
+  };
+  componentDidMount() {
+    getDecks().then((result) => {
+      console.log(result);
+      this.setState(() => ({
+        data: result,
+      }));
+    });
+  }
+  render() {
+    const { data } = this.state;
+    return (
+      <View style={styles.container}>
+        <Text>{JSON.stringify(data)}</Text>
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
