678ef5bfabaa038abaffae0e22eb6b56d736cf10
crisanlucid <crisan.lucid@yahoo.com>
2020-04-26 22:26:11 +0200

    feat: Add reducers

---
+++ reducers/index.js
@@ -0,0 +1,42 @@
+import {
+  RECEIVE_DECKS,
+  ADD_DECK,
+  REMOVE_DECK,
+  ADD_CARD,
+} from '../actions/index';
+import { formatId } from '../utils/helpers';
+
+export default function decks(state = {}, action) {
+  switch (action.type) {
+    case RECEIVE_DECKS:
+      return {
+        ...state,
+        ...action.decks,
+      };
+    case ADD_DECK:
+      const { title } = action;
+      const id = formatId(title);
+      return {
+        ...state,
+        [id]: {
+          title,
+          questions: [],
+        },
+      };
+    case REMOVE_DECK:
+      const { id } = action;
+      const { [id]: value, ...restDecks } = state;
+      console.log(restDecks);
+      return restDecks;
+    case ADD_CARD:
+      const { deckId, card } = action;
+      return {
+        ...state,
+        [deckId]: {
+          questions: [...state[deckId].questions].concat(card),
+        },
+      };
+    default:
+      return state;
+  }
+}
